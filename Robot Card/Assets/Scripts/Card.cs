74221f47b39a0fac7c30b392466ecc11567f80c1
TMunt <t.munt@lancaster.ac.uk>
2019-01-03 22:17:17 +0000

    Added new art, create scritable object cards

---
+++ Robot Card/Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+	public string cardName;
+	public Sprite artwork;
+	public int speed;
+	public int agility;
+	public int experience;
+	public int defence;
+	public int attack;
+
+
+}
---
+++ Robot Card/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+
+	public Text nameText;
+	public Text speed;
+	public Text defence;
+	public Text agility;
+	public Text attack;
+	public Text experience;
+
+	public Image artwork;
+	
+	// Use this for initialization
+	void Start () {
+		
+
+		nameText.text = card.cardName;
+		speed.text = card.speed.ToString();
+		defence.text = card.defence.ToString();
+		agility.text = card.agility.ToString();
+		attack.text = card.attack.ToString();
+		experience.text = card.experience.ToString();
+		artwork.sprite = card.artwork;
+	}
+	
+	
+}
